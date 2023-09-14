1382b86c3f733b46c1f76fc6c45a48e826787c4d
Jayksss <bulnanli@gmail.com>
2023-09-14 20:47:59 +0900

    init

---
+++ src/main/java/com/ypmall/YPmallApplication.java
@@ -0,0 +1,13 @@
+package com.ypmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YPmallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YPmallApplication.class, args);
+	}
+
+}

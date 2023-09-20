c0fe92905240f4ca5c34a430936b809eddc1ae6b
YEWON <73874829+YEWONdevelopment@users.noreply.github.com>
2023-09-20 20:56:44 +0900

    thymeleaf 의존성추가
    thymeleaf 의존성추가

--- src/main/java/com/ypmall/MainController.java
+++ src/main/java/com/ypmall/MainController.java
@@ -1,13 +1,17 @@
 package com.ypmall;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class MainController {
 
-    @GetMapping("/")
-    public String main () {
-        return "YPmall";
+    @RequestMapping("/")
+    public String hello () {
+        return "index";
     }
 }
